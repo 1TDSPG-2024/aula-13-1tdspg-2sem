47f83876dc846395b190b6b4a7c6f5947e8798c1
LeonardoIafrate <leonardoiafrate01@gmail.com>
2024-09-20 09:53:00 -0300

    Criando rotas estruturadas e parametrizadas

---
+++ aula13/aula13/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,12 @@
+import Menu from "../Menu/Menu";
+
+export default function Cabecalho() {
+    return(
+    <header>
+        <div>
+            <h1>Cabecalho</h1>
+            <Menu/>
+        </div>
+    </header>
+    )
+}
