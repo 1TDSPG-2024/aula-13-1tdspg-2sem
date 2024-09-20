2ef3afb70b1afd8b033225b8bb4c7bc1283c3400
anacarolcg0 <anacarolcg0@gmail.com>
2024-09-20 09:54:39 -0300

    Criando rotas estruturadas e parameterizadas

--- aula13/src/app/page.tsx
+++ aula13/src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
-  return(
-    <div>Página Principal</div>
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
   )
 }
---
+++ aula13/src/components/Rodape/Rodape.tsx
@@ -0,0 +1,9 @@
+
+export default function Rodape() {
+    return (
+        <footer>
+            <h2>Rodape</h2>
+            <p>Desenvolvido por <span>ABC</span></p>
+        </footer>
+    )
+}
