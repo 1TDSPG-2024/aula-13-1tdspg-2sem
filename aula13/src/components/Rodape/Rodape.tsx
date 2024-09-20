6f6aa9fed7a1c6b2d52a0142fefefc634641a8f7
Samir Neto <samihneto@gmail.com>
2024-09-20 09:52:42 -0300

    Criando rotas estruturadas e parametrizadas

---
+++ aula13/src/components/Rodape/Rodape.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Rodape() {
+  return (
+    <footer>
+        <h2>Rodapé</h2>
+        <p>Desenvolvido por Samir Neto em 20/09/2024</p>
+    </footer>
+    
+  )
+}
